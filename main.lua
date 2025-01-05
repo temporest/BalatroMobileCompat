101d8466f2e26a82597be47ee86f72e3077e79e6
avery <143365819+eeve-lyn@users.noreply.github.com>
2025-01-05 20:55:10 -0300

    the actual mod tm

---
+++ main.lua
@@ -0,0 +1,5 @@
+--i was legally obligated to make a main file
+--in order to have steamodded metadata
+--so yay have this even though this mod is just lovely patches
+
+MobileCompat = SMODS.current_mod
